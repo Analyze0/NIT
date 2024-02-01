33d58153cc1e73b29849f830219766f147e80154
Analyze0 <101422486+Analyze0@users.noreply.github.com>
2024-02-01 22:47:30 +0000

    Initial commit

---
+++ pages/alumni/script.js
@@ -0,0 +1,6 @@
+document.querySelector('.title').onmousedown = function(e){
+  window.location.href = "https://nit.analyze185.repl.co";
+}
+document.querySelector('.logo').onmousedown = function(e){
+  window.location.href = "https://nit.analyze185.repl.co";
+}
---
+++ pages/campus-life/script.js
@@ -0,0 +1,24 @@
+document.querySelector('.title').onmousedown = function(e) {
+  window.location.href = "https://nit.analyze185.repl.co";
+}
+document.querySelector('.logo').onmousedown = function(e) {
+  window.location.href = "https://nit.analyze185.repl.co";
+}
+
+var toggle = false;
+
+document.getElementById('video').onmousedown = function(e) {
+  var video = document.getElementById("video");
+  video.muted = !video.muted;
+
+  if (toggle == true) {
+    document.getElementById('video').style.cursor = "url(files/play.cur),auto";
+    video.pause();
+    toggle = false;
+  }
+  else {
+    document.getElementById('video').style.cursor = "url(files/pause.cur),auto";
+    video.play();
+    toggle = true;
+  }
+}
